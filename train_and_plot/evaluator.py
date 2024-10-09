ad69437f5920d23ba6dfbae6f003ddc926cbbbb6
HugoPlatzer <hugoplatzer@aon.at>
2024-10-09 12:06:04 +0200

    basic evaluator functionality

---
+++ train_and_plot/evaluator.py
@@ -0,0 +1,35 @@
+import torch
+
+class Evaluator:
+    def __init__(self, sampler, model, num_samples):
+        self.sampler = sampler
+        self.model = model
+        self.num_samples = num_samples
+    
+    def __str__(self):
+        return (f"{self.__class__.__name__}("
+        f"sampler={self.sampler}, "
+        f"model={self.model}], "
+        f"num_samples={self.num_samples})"
+        )
+    
+    def evaluate_model(self):
+        prompts_responses = [self.sampler.get_prompt_and_response()
+            for i in range(self.num_samples)]
+        prompts = [a[0] for a in prompts_responses]
+        correct_responses = [a[1] for a in prompts_responses]
+        model_responses = self.model.answer_prompts(prompts)
+        if not len(prompts) == len(correct_responses) == len(model_responses):
+            raise ValueError("wrong number of responses from model")
+        total_answers = len(model_responses)
+        good_answers = 0
+        for prompt, correct_response, model_response in zip(
+            prompts, correct_responses, model_responses):
+            print(f"prompt={repr(prompt)}, "
+                f"correct_response={repr(correct_response)}, "
+                f"model_response={repr(model_response)}")
+            if model_response == correct_response:
+                good_answers += 1
+        accuracy = good_answers / total_answers
+        return accuracy
+        
---
+++ train_and_plot/test_evaluator.py
@@ -0,0 +1,17 @@
+import sampler
+import model
+import evaluator
+
+sampler = sampler.SamplerStringReverse(max_len=5, mixed_len=True)
+model = model.GPT2Model(
+    max_prompt_len=sampler.get_max_prompt_len(),
+    max_response_len=sampler.get_max_response_len(),
+    n_embd=128,
+    n_layer=2,
+    n_head=2,
+)
+
+e = evaluator.Evaluator(sampler, model, num_samples=10)
+print(e)
+accuracy = e.evaluate_model()
+print(accuracy)
