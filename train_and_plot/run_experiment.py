85b2e1c06e5c09dbfa14187b16adfc8566c9e4c1
HugoPlatzer <hugoplatzer@aon.at>
2024-10-15 01:48:50 +0200

    automatic model saving, plot generation

--- train_and_plot/run_experiment.py
+++ train_and_plot/run_experiment.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 import experiment
+import plot_generator
 
 
 if len(sys.argv) != 2:
@@ -12,7 +13,14 @@ if len(sys.argv) != 2:
 experiment_path = sys.argv[1]
 experiment_configfile = os.path.join(experiment_path, "config.json")
 experiment_resultsfile = os.path.join(experiment_path, "results.json")
+experiment_modelfile = os.path.join(experiment_path, "model.bin")
+experiment_plotfile = os.path.join(experiment_path, "plot.pdf")
 
 experiment_ = experiment.Experiment(experiment_configfile)
 experiment_.run_training()
 experiment_.save_results(experiment_resultsfile)
+
+experiment_.model.save_to_file(experiment_modelfile)
+
+plotgen = plot_generator.PlotGenerator(experiment_resultsfile)
+plotgen.generate_plot(experiment_plotfile)
