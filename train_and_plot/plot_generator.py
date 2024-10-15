9cc18c91e88ffdfcacba898e019fbc31de5d3c3b
HugoPlatzer <hugoplatzer@aon.at>
2024-10-16 00:46:54 +0200

    proper zorder in plot

--- train_and_plot/plot_generator.py
+++ train_and_plot/plot_generator.py
@@ -1,5 +1,6 @@
 import json
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 class PlotGenerator:
     def __init__(self, json_file):
@@ -25,17 +26,36 @@ class PlotGenerator:
         y_values_acc = [ts["accuracy"]
             for ts in self.json_data["training_states"]]
         
-        fig, ax1 = plt.subplots()
-        ax1.plot(x_values, y_values_loss, color="blue", label="Loss")
-        ax1.set_xlim(0, max(x_values) * 1.05)
-        ax1.set_ylim(0, max(y_values_loss) * 1.05)
-        ax1.set_xlabel("Training step")
-        ax1.set_ylabel("Loss")
+        plt.style.use("ggplot")
+        loss_color = (plt.rcParams['axes.prop_cycle'].by_key()
+            ['color'][1])
+        accuracy_color = (plt.rcParams['axes.prop_cycle'].by_key()
+            ['color'][0])
         
-        ax2 = ax1.twinx()
-        ax2.plot(x_values, y_values_acc, color="red", label="Accuracy")
-        ax2.set_ylim(-0.05, 1.05)
-        ax2.set_ylabel("Accuracy")
+        fig = plt.figure()
+        gs = gridspec.GridSpec(1, 1)
+        
+        ax1 = fig.add_subplot(gs[0, 0], zorder=1)
+        ax1.set_xlim(0, max(x_values) * 1.05)
+        ax1.set_ylim(-0.05, 1.05)
+        ax1.grid(True)
+        ax1.tick_params(bottom=False, labelbottom=False,
+            left=False, labelleft=False)
+        
+        ax2 = fig.add_subplot(gs[0, 0], zorder=2, frame_on=False)
+        ax2.plot(x_values, y_values_loss, label="Loss", color=loss_color)
+        ax2.set_xlim(0, max(x_values) * 1.05)
+        ax2.set_ylim(0, max(y_values_loss) * 1.05)
+        ax2.set_xlabel("Training step")
+        ax2.set_ylabel("Loss")
+        ax2.grid(False)
+        
+        ax3 = ax2.twinx()
+        ax3.plot(x_values, y_values_acc, color="red", label="Accuracy")
+        ax3.set_xlim(0, max(x_values) * 1.05)
+        ax3.set_ylim(-0.05, 1.05)
+        ax3.set_ylabel("Accuracy")
+        ax3.grid(False)
         
         plt.tight_layout()
         plt.savefig(output_file)
