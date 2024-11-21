026959bd3a8a43cfb2e7c5d0ee4d44ec15b44aff
HugoPlatzer <hugoplatzer@aon.at>
2024-11-21 21:29:15 +0100

    plotting of final test accuracy

--- train_and_plot_v2/plotgen/__init__.py
+++ train_and_plot_v2/plotgen/__init__.py
@@ -1,2 +1,3 @@
 from . import plot_experiment
 from . import plot_accuracy
+from . import plot_final_accuracy
---
+++ train_and_plot_v2/plotgen/plot_final_accuracy.py
@@ -0,0 +1,47 @@
+import json
+import os
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FixedLocator, MultipleLocator, FixedFormatter
+import numpy as np
+
+def create_plot(config_file):
+    config = json.loads(open(config_file).read())
+    
+    x_tick_labels = config["x_axis_points"]
+    x_values = list(range(len(x_tick_labels)))
+    
+    curves = []
+    for curve in config["curves"]:
+        y_values = []
+        for results_file in curve["results_files"]:
+            results_data = json.loads(open(results_file).read())
+            test_accuracy = results_data["best_model"]["test_accuracy"]
+            y_values.append(test_accuracy)
+        
+        curve_data = {
+            "name": curve["name"],
+            "y_values": y_values
+        }
+        curves.append(curve_data)
+    
+    fig, ax = plt.subplots()
+    
+    colors = plt.colormaps["tab10"].colors
+    
+    for i, curve in enumerate(curves):
+        ax.plot(x_values, curve["y_values"], label=curve["name"],
+            marker="s", color=colors[i])
+    
+    ax.xaxis.set_major_locator(FixedLocator(x_values))
+    ax.xaxis.set_major_formatter(FixedFormatter(x_tick_labels))
+    ax.yaxis.set_major_locator(MultipleLocator(0.2))
+    plt.ylim(-0.05, 1.05)
+    
+    plt.xlabel(config["x_axis_label"])
+    plt.ylabel("Accuracy")
+    
+    plt.grid(True)
+    plt.legend(loc="lower right", bbox_to_anchor=(1,1))
+    plt.tight_layout()
+    output_file = config_file.rsplit(".", 1)[0] + ".pdf"
+    plt.savefig(output_file)
